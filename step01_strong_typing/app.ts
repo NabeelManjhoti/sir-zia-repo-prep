7591eb0bc762992526c6df6ccf02a9efa107ca66
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-07-11 22:59:15 +0500

    first commit

---
+++ step01_strong_typing/app.ts
@@ -0,0 +1,13 @@
+//Strongly typed Syntax.
+  let myName:string = "Nabeel";
+  let myAge:number = 27;
+  let isStudent:boolean = true;
+  let myEmail:any = "xyz@example.com";
+
+//Type Infrence.
+  let myName2 = "Rehman";
+  let myAge2 = 20;
+  let isEmployee = false;
+  let myEmail2 = "abc@example.com";
+
+  
