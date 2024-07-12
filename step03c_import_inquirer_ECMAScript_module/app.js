86419fd198fe218019da37df133b9daeaf56173d
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-07-12 23:17:58 +0500

    first commit

---
+++ step03c_import_inquirer_ECMAScript_module/app.js
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        name: "age",
+        type: "number",
+        message: "Enter your Age"
+    },
+]);
+let targetAge = 60;
+console.log(`Insha Allah, in ${60 - answer.age} years, I will be ${targetAge} year Old`);
