48803c55bfbe69d1d4976ad12befa49c90818711
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-07-16 13:53:08 +0500

    first commit

---
+++ step03d_chalk/app.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let targetAge:number = 60;
+
+let answer = await inquirer.prompt([
+    {
+        name: "age",
+        type: "number",
+        message: "Enter your Age"
+    },
+]);
+
+console.log(chalk.yellow(`Insha Allah, In next ${60 - answer.age} years, I will be ${targetAge} years Old`));
---
+++ step04_unions_literals/app.ts
@@ -0,0 +1,77 @@
+let myname: string | null;
+
+myname = null;
+console.log(myname);
+
+myname = "Nabeel";
+console.log(myname);
+
+//myname = undefined; //Error
+//myname = 12; //Error
+
+let myAge:string | number;
+
+myAge = 27; //Narrowing.
+console.log(myAge);
+
+// console.log(myAge.toLowerCase());//Error
+
+myAge = "Don't Know"; //Narrowing.
+console.log(myAge);
+
+console.log(myAge.toString()); // common to both types 
+//                                //can be called even without narrowing
+
+console.log(myAge.toLowerCase());//Can be called on string  
+//                                 //because of narrowing
+
+let newAge = Math.random() > 0.6 ? "Nabeel" : 5;
+
+// newAge.toLowerCase() //Error: Transpilor cannot Arrow.
+
+if(newAge === "Nabeel"){
+    newAge.toUpperCase();
+};
+
+if(typeof newAge === "string"){
+    newAge.toUpperCase();
+};
+
+typeof newAge === "string"
+? newAge.toUpperCase() //Ok: String
+: newAge.toFixed() //Ok: Number
+
+let age: number | "died" | "unknown";
+
+age = 90;//OK
+age = "died";//OK
+age = "unknown";//OK
+// age = "living";//Error
+
+
+let zia: "zia";
+
+zia = "zia";
+// zia = "khan";//Error
+
+
+let yourName = Math.random() > 0.6 ? "Hira Khan": undefined;
+
+if (yourName) {
+    yourName.toUpperCase(); // Ok: string
+}
+
+//yourName.toUpperCase();//Error: Object is possibly 'undefined'.
+
+yourName?.toUpperCase();//OK
+
+// You can also define a type alias
+type RawData = boolean | number | string | null | undefined;
+
+let data: RawData;
+
+// You can even combine them
+
+type Id = number | string;
+
+type IdMaybe = Id | undefined | null;
