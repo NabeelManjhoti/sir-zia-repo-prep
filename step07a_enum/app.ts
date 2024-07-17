e0cc07693a5a69f4dd075186b2e62e63b5bdeefc
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-07-17 15:50:23 +0500

    first commit

---
+++ step07a_enum/app.ts
@@ -0,0 +1,23 @@
+enum Color {
+    Red,
+    Green,
+    Blue
+};
+let c:Color = Color.Green;
+console.log(c); //output = 1
+
+enum Color1 {
+    Red = 1,
+    Green,
+    Blue
+};
+let colorName:string = Color1[2];
+console.log(colorName); //output = Green
+
+enum Color2 {
+    Red = 1,
+    Green = 2,
+    Blue = 4
+}; //can assign values to all.
+let colorIndex = Color2["Blue"];
+console.log(colorIndex);
