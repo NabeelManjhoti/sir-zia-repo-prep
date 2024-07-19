52d91bff83c3780694824a0cff82bc7742e663d6
Nabeel Ali <160395774+NabeelManjhoti@users.noreply.github.com>
2024-07-19 23:13:59 +0500

    updated comments

--- step09c_function_default_parameter/app.ts
+++ step09c_function_default_parameter/app.ts
@@ -1,4 +1,5 @@
-//Lorem ipsum
+////Named function with optional and default parameters
+//(Note that the parameter type will be optional when used with default value)
   function buildName(firstName:string, lastName:string = "Ali"):string{
     if(lastName)
         return firstName + " " + lastName;
@@ -10,7 +11,8 @@
   let result2 = buildName("Nabeel", "Ali") //Also Works
   let result3 = buildName("Mr", "Nabeel", "Ali"); //Error: too much parameters
 
-//lorem ipsum
+////anonymous function type with defult parameters 
+//(Note that the parameter type will be optional when used with defult value)
   let buildName1:(firstName:string, lastName?:string) => string = 
   function(firstName:string, lastName:string = "Ali"):string{
     if(lastName)
